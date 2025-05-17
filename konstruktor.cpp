4e7ba36e856000319dcc6a4ff92aafe9d022f352
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 18:08:27 +0700

    class buku public commit

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -60,3 +60,16 @@ private:
     string penulis;
     bool dipinjam;
 
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void tampilkanInfo() {
+        cout << "Judul: " << judul 
+             << ", Penulis: " << penulis 
+             << ", Status: " << (dipinjam ? "Dipinjam" : "Tersedia") 
+             << endl;
+    }
+
+    friend class Petugas;
+};
+
