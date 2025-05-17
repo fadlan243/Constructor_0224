9b5fbfd08f3a9eaa969f9149fb32712bec3804e3
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 17:33:02 +0700

    class mahasiswa commit

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -14,38 +14,3 @@ mahasiswa(int Nim, string iNama);
 void cetak();
 };
 
-mahasiswa :: mahasiswa(){
-    nim = 0;
-    nama = "";
-}
-
-mahasiswa :: mahasiswa(int iNim){
-nim=iNim;
-}
-
-mahasiswa :: mahasiswa(string nama){
-nama=iNama;
-}
-
-mahasiswa :: mahasiswa(int iNim, string iNama){
-nim=iNim;
-nama=iNama;
-}
-void mahasiswa::cetak(){
-cout<<endl<< "Nim =" <<nim<<endl;
-cout << " Nama = " <<nama<endl;
-}
-
-int main(){
-    mahasiswa mhs1;
-    mahasiswa mhs2(20);
-    mahasiswa mhs3("Indra");
-    mahasiswa mhs4(30, "Fauzan");
-
-    mhs1.cetak();
-    mhs2.cetak();
-    mhs3.cetak();
-    msh4.cetak();
-
-    return 0;
-}
