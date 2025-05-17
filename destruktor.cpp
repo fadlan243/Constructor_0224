45d14f0ab5ca6ff4d2490436a1260e401d634bc8
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 17:59:20 +0700

    class buku commit

--- destruktor.cpp
+++ destruktor.cpp
@@ -49,3 +49,22 @@ int main(){
 // Forward declaration
 class Petugas;
 
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void tampilkanInfo() {
+        cout << "Judul: " << judul 
+             << ", Penulis: " << penulis 
+             << ", Status: " << (dipinjam ? "Dipinjam" : "Tersedia") 
+             << endl;
+    }
+
+    friend class Petugas; // Petugas boleh mengubah status peminjaman
+};
+
