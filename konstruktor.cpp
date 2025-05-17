a4c398ea6f352396c3e2fea62f51cd16d3bd7117
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 18:07:53 +0700

    class buku private commit

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -27,3 +27,36 @@ mahasiswa :: mahasiswa(string nama){
 nama=iNama;
 }
 
+mahasiswa :: mahasiswa(int iNim, string iNama){
+nim=iNim;
+nama=iNama;
+}
+void mahasiswa::cetak(){
+cout<<endl<< "Nim =" <<nim<<endl;
+cout << " Nama = " <<nama<endl;
+}
+
+int main(){
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    msh4.cetak();
+
+    return 0;
+}
+
+// Forward declaration
+class Petugas;
+class Admin;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
