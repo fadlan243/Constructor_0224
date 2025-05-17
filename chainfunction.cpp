d9573b92b457a069ec5921af9048f0738066626d
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 17:50:41 +0700

    petugas boleh akses private commit

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -28,3 +28,23 @@ int main()
 // Forward declaration
 class Petugas;
 
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void tampilkanInfo() {
+        cout << "Judul: " << judul 
+             << ", Penulis: " << penulis 
+             << ", Status: " << (dipinjam ? "Dipinjam" : "Tersedia") 
+             << endl;
+    }
+
+    // Petugas boleh akses private
+    friend class Petugas;
+};
+
