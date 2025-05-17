6ff4b1cc85ba7cf672326598ac189d0b4f2de52d
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 18:09:32 +0700

    class peminjam commit

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -73,3 +73,25 @@ public:
     friend class Petugas;
 };
 
+class Peminjam {
+private:
+    string nama;
+    int id;
+    int totalPinjaman;
+
+public:
+    Peminjam(string n, int i) : nama(n), id(i), totalPinjaman(0) {}
+
+    void tampilkanInfo() {
+        cout << "Nama: " << nama 
+             << ", ID: " << id 
+             << ", Total Pinjaman: " << totalPinjaman 
+             << endl;
+    }
+
+    string getNama() { return nama; }
+    int getId() { return id; }
+
+    friend class Petugas;
+};
+
