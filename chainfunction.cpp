224c86a802893f270519779c0bb48ad498b3152f
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-17 16:36:58 +0700

    bukunya commit

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -17,10 +17,3 @@ publik
 
 } bukunya;
 
-int main()
-{
-    // bukunya.setJudul("Matematika");
-    // cout << bukunya.getJudul();
-    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
-    return 0;
-}
